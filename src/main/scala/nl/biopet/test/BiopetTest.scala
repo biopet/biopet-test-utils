e96e160b9ba0dec04d1a2c8700c6f27842fe2134
pjvan_thof <pjrvanthof@gmail.com>
2017-10-04 11:46:23 +0200

    Adding resource method

--- src/main/scala/nl/biopet/test/BiopetTest.scala
+++ src/main/scala/nl/biopet/test/BiopetTest.scala
@@ -1,8 +1,12 @@
 package nl.biopet.test
 
+import java.nio.file.Paths
+
 import org.scalatest.Matchers
 import org.scalatest.testng.TestNGSuite
 
 trait BiopetTest extends TestNGSuite with Matchers {
-
+  def resourcePath(p: String): String = {
+    Paths.get(this.getClass.getResource(p).toURI).toString
+  }
 }
