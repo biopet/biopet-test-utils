609bfaf9cd9025b23f8f559b9528098714259283
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-17 22:49:06 +0200

    Test resource method

--- src/test/scala/nl/biopet/test/TraitTest.scala
+++ src/test/scala/nl/biopet/test/TraitTest.scala
@@ -1,13 +1,27 @@
 package nl.biopet.test
 
+import java.io.File
+
 import org.scalatest.Matchers
 import org.scalatest.testng.TestNGSuite
 import org.testng.annotations.Test
+
+import scala.io.Source
 
 class TraitTest extends BiopetTest {
   @Test
   def testClass(): Unit = {
     assert(this.isInstanceOf[TestNGSuite])
     assert(this.isInstanceOf[Matchers])
+  }
+
+  @Test
+  def testResourcePath(): Unit = {
+    val path = resourcePath("/test.txt")
+    Source.fromFile(new File(path)).mkString shouldBe "test"
+
+    intercept[IllegalStateException] {
+      resourcePath("/does_not_exist.file")
+    }.getMessage shouldBe "Resource '/does_not_exist.file' not found"
   }
 }
