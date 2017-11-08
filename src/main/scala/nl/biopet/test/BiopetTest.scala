d031d90fe15cc7f49e4eca7395ee7376fd475ffe
pjvan_thof <pjrvanthof@gmail.com>
2017-11-08 13:30:11 +0100

    Format issues

--- src/main/scala/nl/biopet/test/BiopetTest.scala
+++ src/main/scala/nl/biopet/test/BiopetTest.scala
@@ -9,7 +9,8 @@ import org.scalatest.testng.TestNGSuite
 trait BiopetTest extends TestNGSuite with Matchers {
   def resourcePath(p: String): String = {
     val resource = this.getClass.getResource(p)
-    if (resource == null) throw new IllegalStateException(s"Resource '$p' not found")
+    if (resource == null)
+      throw new IllegalStateException(s"Resource '$p' not found")
     else Paths.get(this.getClass.getResource(p).toURI).toString
   }
 
